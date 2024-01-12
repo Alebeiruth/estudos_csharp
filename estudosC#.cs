dd172b6e4ecfeefb125afb71d349b9f63ef46df2
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-12 19:48:41 -0300

    Update estudosC#.cs

--- estudosC#.cs
+++ estudosC#.cs
@@ -1,3 +1,18 @@
+// desafio declaração das variaveis
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+
+
 //DESAFIO C# utilizadno strings, @, $, \n, \t
 
 string projectName = "ACME";
