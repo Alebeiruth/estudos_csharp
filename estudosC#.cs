a4e94f4e17acf6ae048123f08cdc0f241f8b7b71
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-12 21:10:14 -0300

    Update estudosC#.cs

--- estudosC#.cs
+++ estudosC#.cs
@@ -129,3 +129,48 @@ int fahrenheit = 94;
 decimal celsius = (fahrenheit - 32m) * (5m / 9m);
 Console.WriteLine("The temperature is " + celsius + " Celsius.");
 
+//PROJETO 01
+// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+string student1 = "Sophia";
+string student2 = "Nicolas";
+string student3 = "Zahirah";
+string student4 = "Jeong";
+
+float sophia = (float)(sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / currentAssignments;
+float nicolas = (float)(nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / currentAssignments;
+float zahirah = (float)(zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / currentAssignments;
+float jeong = (float)(jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / currentAssignments;
+
+Console.WriteLine($"Student \tGrade\n{student1}\t\t{sophia} " + " A" + $"\n{student2}\t\t{nicolas} " + " B" + $"\n{student3}\t\t{zahirah} " + " C" + $"\n{student4}\t\t{jeong} " + " D");
+
+
+
+
+
+
