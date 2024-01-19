3a5d7a48bfe9804f02953525e35e8732cf56d35c
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-19 18:42:13 -0300

    Update estudosC#.cs

--- estudosC#.cs
+++ estudosC#.cs
@@ -714,5 +714,74 @@ foreach (string item in items)
     Console.WriteLine(item);
 }
 
+//FORMATAÇÃO COMPOSTA
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+// FORMATAÇÃO INTERPOLAÇÃO
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+
+//FORMATAÇÃO MOEDA CORRENTE
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+//FORMATAR NUMEROS
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+Console.WriteLine($"Measurement: {measurement:N4} units");
+
+//FORMATA PORCETAGEM
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+//FORMATAÇÃO COMBINADA
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+
+//EXERCICIO 01
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+//FORMATA COM ESPAÇO EM BRANCO A ESQUERDA
+string input = "Pad this";
+ Console.WriteLine(input.PadLeft(12));
+ Console.WriteLine(input.PadLeft(12, '-'));
+ Console.WriteLine(input.PadRight(12, '-'));
+
+
+ //FORMATA COM ESPAÇO EM BRANCO A DIREITA
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
+
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine);
+
+
 
 
