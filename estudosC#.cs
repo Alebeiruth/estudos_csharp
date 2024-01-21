52ee8c1cc0c9273ec4499870252c97a10d596deb
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-21 12:04:13 -0300

    Update estudosC#.cs

--- estudosC#.cs
+++ estudosC#.cs
@@ -1128,5 +1128,36 @@ for (int i = 0; i < times.Length; i++)
 
 }
 
+//EXEMPLO 03
+double pi = 3.14159;
+PrintCircleArea(12);
+PrintCircleCircumference(12);
+PrintCircleInfo(12);
+PrintCircleInfo(24);
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
+
+void PrintCircleInfo(int radius) 
+{
+    Console.WriteLine($"Circle with radius {radius}");
+    PrintCircleArea(radius);
+    PrintCircleCircumference(radius);
+}
+
+
+
+
+
+
 
 
